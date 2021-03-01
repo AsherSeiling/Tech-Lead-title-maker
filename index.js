6e419e81e96eee0424a74eebabc067f5eb6eca80
Asher Seiling <f14asher@gmail.com>
2021-03-01 08:48:39 -0600

    Revamp using Jquery

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 // © 2020 Asher Seiling
 
 // Display title variable
-var title_create = "";
+let title_create = "";
+// Aray of add to title
+let add_to_title = ["(addressing my haters)", "(Millionare point of view)", "As a millionare","(As a millionare)", "(Dissing the haters)"];
 
 // Main function
 function generate_title(){
-    const prefix_title = document.getElementById("prefix_text").value;
-    const random_select_num = Math.floor(Math.random() * 4) + 1;
-    var add_to_title = "";
-    if(random_select_num == 1){
-        add_to_title = "(As a millionare)";
-    }
-    else if(random_select_num == 2){
-        add_to_title = "As a millionare";
-    }
-    else if(random_select_num == 3){
-        add_to_title = "(Millionare point of view)";
-    }
-    else if(random_select_num == 4){
-        add_to_title = "(addressing my haters)";
-    }
-
-    title_create = prefix_title + " " + add_to_title;
-
+    const prefix_title = $("#prefix_text").val();
+    const random_select_num = Math.floor(Math.random() * add_to_title.length) + 0;
+    title_create = prefix_title + " " + add_to_title[random_select_num];
     // Return result
     return_res();
 
@@ -32,6 +19,6 @@ function generate_title(){
 // Funciton that returns the result
 
 function return_res(){
-    document.getElementById("the_title_is").innerHTML = "The Tech Lead name for this is";
-    document.getElementById("disp_result").innerHTML = title_create;
+    $("#the_title_is").html("The Tech Lead name for this is");
+    $("#disp_result").html(title_create);
 }
